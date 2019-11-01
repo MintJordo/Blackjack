e1a360750c5656d211c6095bd748ca423644c175
Jordan Oberg <jordan.oberg@trojans.dsu.edu>
2019-11-01 14:11:30 -0500

    Added Features
    Shuffles all cards back into the deck after each hand
    Balance lasts between executions

--- BlackJack/BlackJack/Form1.cs
+++ BlackJack/BlackJack/Form1.cs
@@ -30,7 +30,6 @@ namespace BlackJack
         int betInc = 5;
         string cardBackFileLocation = @"..\..\..\..\cards\red_back.png";
 
-
         public void updatePlayerHandPictureBox()
         {
             if (player1.hand.show().Length >= 1)
@@ -161,8 +160,7 @@ namespace BlackJack
 
             player1.hand.emptyHand();
             dealer.hand.emptyHand();
-
-
+            deck.shuffle();
         }
 
         /* This function intercepts all the commands sent to the application. 
@@ -189,6 +187,7 @@ namespace BlackJack
             InitializeComponent();
             this.FormBorderStyle = FormBorderStyle.None;
             Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 15, 15));
+            moneyBal.Text = System.IO.File.ReadAllText(@"..\..\..\Money.txt");
 
             hitButton.Visible = false;
             standButton.Visible = false;
@@ -390,6 +389,7 @@ namespace BlackJack
 
         private void CloseButton_Click(object sender, EventArgs e)
         {
+            System.IO.File.WriteAllText(@"..\..\..\Money.txt", moneyBal.Text);
             Close();
         }
 
