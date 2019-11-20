155d3754e5351aa69e4a44300f19ca4a43c0b808
1ndy <33045591+1ndy@users.noreply.github.com>
2019-11-19 18:12:36 -0600

    Added Unit Test

---
+++ BlackJack/UnitTestPlayerClass/UnitTestPlayer.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using BlackJack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestPlayerClass
+{
+    [TestClass]
+    public class UnitTestPlayerMoneyManipulation
+    {
+        [TestMethod]
+        public void TestPlayerClassInstantiateWithMoney()
+        {
+            //Arrange
+            Player p1 = new Player(100);
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestPlayerClassAddMoney()
+        {
+            //Arrange
+            Player p1 = new Player(100);
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestPlayerClassRemoveMoney()
+        {
+            //Arrange
+            Player p1 = new Player(100);
+            //Act
+            //Assert
+        }
+    }
+}
