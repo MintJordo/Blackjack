c9fe160e1566b7cc5ed10a41a98a2edbd48a3f34
1ndy <33045591+1ndy@users.noreply.github.com>
2019-12-09 12:23:21 -0600

    Deck Unit Test
    Added unit test for deck class

---
+++ BlackJack/UnitTestDeckClass/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestDeckClass")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestDeckClass")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3c3b3a60-7d5b-4e2c-906a-0998fa53ed4f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BlackJack/UnitTestDeckClass/UnitTestDeckClass.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BlackJack;
+
+namespace UnitTestDeckClass
+{
+    [TestClass]
+    public class UnitTestDeckClass
+    {
+        [TestMethod]
+        public void UnitTestGetCardRepeatedlyFromDeck()
+        {
+            //arrange
+            Deck d = new Deck();
+            Card c1;
+            Card c2;
+            Card c3;
+            Card c4;
+            Card c5;
+            //act
+            c1 = d.getCard();
+            c2 = d.getCard();
+            c3 = d.getCard();
+            c4 = d.getCard();
+            c5 = d.getCard();
+            //assert
+            Assert.AreNotEqual(c1, c2);
+            Assert.AreNotEqual(c1, c3);
+            Assert.AreNotEqual(c1, c4);
+            Assert.AreNotEqual(c1, c5);
+
+            Assert.AreNotEqual(c2, c3);
+            Assert.AreNotEqual(c2, c4);
+            Assert.AreNotEqual(c2, c5);
+
+            Assert.AreNotEqual(c3, c4);
+            Assert.AreNotEqual(c3, c5);
+
+            Assert.AreNotEqual(c4, c5);
+        }
+
+        [TestMethod]
+        public void UnitTestGetCardFromDeckThenShuffleAndRedraw()
+        {
+            //this test may fail on rare occasion but is unlikely
+            //arrange
+            Deck d = new Deck();
+            Card c1, c6;
+            Card c2, c7;
+            Card c3, c8;
+            Card c4, c9;
+            Card c5, c10;
+            //act
+            c1 = d.getCard();
+            c2 = d.getCard();
+            c3 = d.getCard();
+            c4 = d.getCard();
+            c5 = d.getCard();
+
+            d.shuffle();
+
+            c6 = d.getCard();
+            c7 = d.getCard();
+            c8 = d.getCard();
+            c9 = d.getCard();
+            c10 = d.getCard();
+
+            //assert
+            Assert.AreNotEqual(c1, c6);
+            Assert.AreNotEqual(c2, c7);
+            Assert.AreNotEqual(c3, c8);
+            Assert.AreNotEqual(c4, c8);
+            Assert.AreNotEqual(c5, c10);
+        }
+    }
+}
